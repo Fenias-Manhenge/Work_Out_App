513821867e6e833da03bb4e48c059955357f55f1
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2024-01-22 22:29:15 +0200

    Calculating Metric unit

--- app/src/main/java/com/example/work_out/Bmi.kt
+++ app/src/main/java/com/example/work_out/Bmi.kt
@@ -3,6 +3,9 @@ package com.example.work_out
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +14,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.work_out.databinding.BmiBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.pow
 
 class Bmi : AppCompatActivity() {
 
     private val binding by lazy { BmiBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,7 +36,7 @@ class Bmi : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding.tbBMI.setNavigationOnClickListener {
-            showADialog()
+            showDialog()
         }
 
         binding.toggleMetrics.check(binding.btnMetricUnit.id)
@@ -37,7 +44,7 @@ class Bmi : AppCompatActivity() {
 
         onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                showADialog()
+                showDialog()
             }
         })
 
@@ -51,7 +58,7 @@ class Bmi : AppCompatActivity() {
         }
     }
 
-    private fun showADialog(){
+    private fun showDialog(){
         MaterialAlertDialogBuilder(this)
             .setCancelable(false)
             .setTitle("Back")
@@ -77,8 +84,89 @@ class Bmi : AppCompatActivity() {
         )
 
         val view = inflateLayout(layoutResID)
+
+        val tvBmiValue = view.findViewById<TextView>(R.id.tvBmiValue)
+        val tvBmiStatus = view.findViewById<TextView>(R.id.tvBmiStatus)
+        val tvBmiDescription = view.findViewById<TextView>(R.id.tvBmiDescription)
+        val etHeight = view.findViewById<EditText>(R.id.et_height)
+        val etWeight = view.findViewById<EditText>(R.id.et_weight)
+        val btnCalculate = view.findViewById<Button>(R.id.btnCalculate)
+
+        btnCalculate.setOnClickListener{
+            if (validateMetricUnit(etHeight, etWeight)) {
+                val height = etHeight.text.toString().toFloat() / 100
+                val weight = etWeight.text.toString().toFloat()
+
+                val bmi = weight / height.pow(2)
+
+                displayBmiResult(bmi, tvBmiValue, tvBmiStatus, tvBmiDescription)
+            }
+        }
+
         view.layoutParams = layoutParams
         binding.mConstraintLayoutMetrics.removeAllViews()
         binding.mConstraintLayoutMetrics.addView(view)
+    }
+
+    private fun validateMetricUnit(etHeight: EditText, etWeight: EditText): Boolean{
+        var validate = true
+
+        if(etHeight.text.toString().isEmpty())
+            validate = false
+        else if(etWeight.text.toString().isEmpty())
+            validate = false
+
+        return validate
+    }
+
+    private fun displayBmiResult(
+        bmi: Float,
+        tvBmiValue: TextView,
+        tvBmiStatus: TextView,
+        tvBmiDescription: TextView
+    ){
+        val bmiLabel: String
+        val bmiDescription: String
+
+        when {
+            bmi <= 15 -> {
+                bmiLabel = resources.getString(R.string.bmiLabelVerySeverelyUnderweight)
+                bmiDescription = resources.getString(R.string.bmiDescriptionUnderWeight)
+            }
+            bmi <= 16 -> {
+                bmiLabel = resources.getString(R.string.bmiLabelSeverelyUnderweight)
+                bmiDescription = resources.getString(R.string.bmiDescriptionUnderWeight)
+            }
+            bmi <= 18.5 -> {
+                bmiLabel = resources.getString(R.string.bmiLabelUnderweight)
+                bmiDescription = resources.getString(R.string.bmiDescriptionUnderWeight)
+            }
+            bmi <= 25 -> {
+                bmiLabel = resources.getString(R.string.bmiLabelNormal)
+                bmiDescription = resources.getString(R.string.bmiDescriptionNormal)
+            }
+            bmi <= 30 -> {
+                bmiLabel = resources.getString(R.string.bmiLabelOverWeight)
+                bmiDescription = resources.getString(R.string.bmiDescriptionObese)
+            }
+            bmi <= 35 -> {
+                bmiLabel = resources.getString(R.string.bmiLabelModeratelyObese)
+                bmiDescription = resources.getString(R.string.bmiDescriptionObese)
+            }
+            bmi <= 40 -> {
+                bmiLabel = resources.getString(R.string.bmiLabelSeverelyObese)
+                bmiDescription = resources.getString(R.string.bmiDescriptionSeverelyObese)
+            }
+            else -> {
+                bmiLabel = resources.getString(R.string.bmiLabelVerySeverelyObese)
+                bmiDescription = resources.getString(R.string.bmiDescriptionSeverelyObese)
+            }
+        }
+
+        val bmiValue = BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
+
+        tvBmiValue.text = bmiValue
+        tvBmiStatus.text = bmiLabel
+        tvBmiDescription.text = bmiDescription
     }
 }
